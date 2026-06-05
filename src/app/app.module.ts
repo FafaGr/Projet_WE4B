9d0cabca0fc74b8672d0a4efcbce8ba5d40ed56f
FafaGr <grief.fares@gmail.com>
2026-06-05 15:41:56 +0200

    Début avec quelques composants de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { BannerBgComponent } from './banner-bg/banner-bg.component';
+import { FilterSecComponent } from './filter-sec/filter-sec.component';
+import {FormsModule} from "@angular/forms";
+import { GamesGridComponent } from './games-grid/games-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    BottomBarComponent,
+    BannerBgComponent,
+    FilterSecComponent,
+    GamesGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
